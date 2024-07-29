10ca375be5b4d4d28a84da74d98b4eebbe9b1f1b
Gwenaelle Detourne <gdetourn@c1r3p2.42perpignan.fr>
2024-07-29 16:41:59 +0200

    wp-Df+sh-mdb3306-

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -8,7 +8,7 @@ if [ ! -e /var/www/html/wp-config.php ]; then
 			 --dbname=${MYSQL_DATABASE} \
 			 --dbuser=${MYSQL_USER} \
 			 --dbpass=${MYSQL_PASSWORD} \
-			 --dbhost=mariadb \
+			 --dbhost=mariadb:3306 \
 			 --path="/var/www/html" \
 
 	 wp core install --allow-root \
@@ -24,14 +24,15 @@ if [ ! -e /var/www/html/wp-config.php ]; then
 			 ${USER1_LOGIN} \
 		 	 ${USER1_EMAIL} \
 			 --user_pass=${USER1_PASS} \
-			 --role="contributor" \
+			 --role="editor" \
 			 --display_name=${USER1_LOGIN} \
 			 --porcelain \
 			 --path="/var/www/html" \
 
 fi
 
-exec /usr/sbin/php-fpm7.4 -F
+exec "$@"
+#exec /usr/sbin/php-fpm7.4 -F
 
 
 #usage: wp user create <user-login> <user-email> [--role=<role>] [--user_pass=<password>] [--user_registered=<yyyy-mm-dd-hh-ii-ss>] [--display_name=<name>] [--user_nicename=<nice_name>] [--user_url=<url>] [--nickname=<nickname>] [--first_name=<first_name>] [--last_name=<last_name>] [--description=<description>] [--rich_editing=<rich_editing>] [--send-email] [--porcelain]
